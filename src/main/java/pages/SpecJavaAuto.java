956dce6da6ab73706280fa4e968ad90ccc015ce8
AlexanderShatalov <abshatalov87@gmail.com>
2022-03-31 09:08:31 +0300

    update homework

---
+++ src/main/java/pages/SpecJavaAuto.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SpecJavaAuto extends BasePage{
+  public SpecJavaAuto(WebDriver driver, String path) {
+        super(driver, "/lessons/qa-auto-java-specialization/");
+    }
+
+    
+}
